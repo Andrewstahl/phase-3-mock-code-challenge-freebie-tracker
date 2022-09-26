c6ebf67f5d52c0190aa2f95f5561fabf4a686ccd
Andrew Stahl <andrewstahl96@gmail.com>
2022-09-26 13:13:43 -0400

    All files working with Ruby. Migrations have been created

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,19 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev, item_name, value)
+    Freebie.create(item_name: item_name, value: value, company_id: self.id, dev_id: dev.id)
+  end
+
+  def self.oldest_company
+    oldest_age = 10000
+    company_id = 0
+    self.all.each do |company|
+      if company.founding_year < oldest_age
+        company_id = company.id
+      end
+    end
+    Company.find(company_id)
+  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,26 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
+
+  def received_one?(item_name)
+    self.freebies.each do |freebie|
+      if freebie.item_name == item_name
+        return true
+      end
+    end
+    return false
+  end
+
+  def give_away(dev, freebie)
+    # Only gives it away if the dev id on the freebie
+    # matches the id on the current dev
+    if freebie.dev_id == self.id
+      freebie.dev = dev
+      freebie.save
+      puts "Thanks for your donation!"
+    else
+      puts "Sorry, that's not yours!"
+    end
+  end
 
 end
